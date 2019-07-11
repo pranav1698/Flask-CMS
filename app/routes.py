1baf3aef399bb7aa6f32df4e475c9317d4903c2c
pranav1698 <hulkcodexl16@gmail.com>
2019-07-12 00:09:45 +0530

    Adding articles

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,9 @@ from app.models import User, Post
 def index():
     posts = db.session.query(Post).all()
     return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+	post = db.session.query(Post).filter_by(id=post_id).first()
+	date = str(post.timestamp)[:10]
+	return render_template('post.html', title=post.title, body=post.body, date=date, username=post.username)
